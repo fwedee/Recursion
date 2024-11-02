6aa2f18f13db53f5b07f2afe1967532359199e47
fw <171364056+fwedee@users.noreply.github.com>
2024-11-02 18:56:52 +0100

    Implementation of Fibonacci sequence

---
+++ Assignment 1/fibonacci.js
@@ -0,0 +1,34 @@
+// Fibonacci sequence implementation
+
+// iterative implementation
+function fibs(n) {
+  let result = Array(n).fill(0);
+  if (n <= 0) {
+    return result;
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (i == 1 || i == 2) {
+      result[i] = 1;
+    } else if (i == 0) {
+      result[i] = 0;
+    } else {
+      result[i] = result[i - 1] + result[i - 2];
+    }
+  }
+  return result;
+}
+
+// recursive implementation
+function fibsRec(n) {
+  if (n <= 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  const fibArray = fibsRec(n - 1);
+  fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+  return fibArray;
+}
+
+console.log(fibsRec(8));
+console.log(fibs(8));
